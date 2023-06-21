eaf2f3a9ad8eff80ebb2fd7eb88eb250d8c382fe
Acbox <850625057@qq.com>
2023-06-21 13:39:51 +0800

    [feat]Add Electron support

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pages: {
+    main: {
+      entry: "src/main.js",
+      template: "public/index.html",
+      filename: "index.html",
+      title: "Main Page"
+    }
+  }
 })
